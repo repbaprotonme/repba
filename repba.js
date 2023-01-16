728f9cfd33fe6da88e5243f1ffd489f9b810252e
Tom Brinkman <reportbase@gmail.com>
2023-01-16 12:59:50 -0700

    na

--- repba.js
+++ repba.js
@@ -2772,8 +2772,8 @@ var thumblst =
     {
         var th = heightobj.getcurrent().getcurrent();
         var headers = headcnv.height*2;
-        var width = rect.width-THUMBORDER*2;
-        var height = rect.height-headers-THUMBORDER*2;
+        var width = rect.width-THUMBORDER;
+        var height = rect.height-headers-THUMBORDER;
         if (width < 0 || height < 0)
             return;
         var r = calculateAspectRatioFit(photo.image.width, photo.image.height, width*th, height*th);
