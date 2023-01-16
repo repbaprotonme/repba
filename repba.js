b4e26188e1a9eb7b449a24adecdd680525305ec7
Tom Brinkman <reportbase@gmail.com>
2023-01-16 07:58:22 -0500

    na

--- repba.js
+++ repba.js
@@ -2244,7 +2244,6 @@ var presslst =
     name: "BOSS",
     pressup: function (context, rect, x, y)
     {
-        thumbobj.enabled = 0;
         context.pinching = 0;
         context.tapping = 0;
         context.isthumbrect = 0;
