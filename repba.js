8c0c51a11bcca8a64c8582646ee11c7f5b735660
Tom Brinkman <reportbase@gmail.com>
2023-01-16 11:06:43 -0700

    na

--- repba.js
+++ repba.js
@@ -3225,9 +3225,9 @@ var templatelst =
     name: "WIDE",
     init: function ()
     {
-        var y = url.searchParams.has("y") ? Number(url.searchParams.get("y")) : 99;
+        var y = url.searchParams.has("y") ? Number(url.searchParams.get("y")) : 50;
         posityobj.set(y);
-        url.height = url.searchParams.has("o") ? Number(url.searchParams.get("o")) : 75;
+        url.height = url.searchParams.has("o") ? Number(url.searchParams.get("o")) : 50;
         url.zoom = url.searchParams.has("z") ? Number(url.searchParams.get("z")) : 0;
         url.slidetop = url.searchParams.has("s") ? Number(url.searchParams.get("s")) : 24;
         url.slidefactor = url.searchParams.has("f") ? Number(url.searchParams.get("f")) : 48;
@@ -3241,9 +3241,9 @@ var templatelst =
     name: "LANDSCAPE",
     init: function (j)
     {
-        var y = url.searchParams.has("y") ? Number(url.searchParams.get("y")) : 99;
+        var y = url.searchParams.has("y") ? Number(url.searchParams.get("y")) : 50;
         posityobj.set(y);
-        url.height = url.searchParams.has("o") ? Number(url.searchParams.get("o")) : 75;
+        url.height = url.searchParams.has("o") ? Number(url.searchParams.get("o")) : ;
         url.zoom = url.searchParams.has("z") ? Number(url.searchParams.get("z")) : 50;
         url.slidetop = url.searchParams.has("s") ? Number(url.searchParams.get("s")) : 24;
         url.slidefactor = url.searchParams.has("f") ? Number(url.searchParams.get("f")) : 48;
