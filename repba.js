73f91fb00080eff3d23cf34ae2cd1c312265bab0
Tom Brinkman <reportbase@gmail.com>
2023-01-18 21:46:21 -0700

    na

--- repba.js
+++ repba.js
@@ -1732,6 +1732,8 @@ var pinchlst =
         context.heightsave = heightobj.getcurrent().getcurrent()
         var obj = zoomobj.getcurrent();
         context.pinchsave = obj.getcurrent()
+        if (context.pinchsave < 2)
+            context.pinchsave = 2;
     },
     pinchend: function (context)
     {
