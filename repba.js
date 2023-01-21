04a05c3858e567541da1e1dbd8a7e036867eb4b2
Tom Brinkman <reportbase@gmail.com>
2023-01-20 20:46:30 -0700

    na

--- repba.js
+++ repba.js
@@ -3720,8 +3720,8 @@ var bodylst =
                                 new Rectangle(context.zoomctrl),
                                 new Fill(THUMBFILL),
                                 new Stroke(THUMBSTROKE,THUMBORDER),
-                                new CurrentVPanel(new Fill(j==0?THUMBDARK:THUMBSTROKE), ALIEXTENT, 1,
-                                    j==0?"WHITE":"BLACK")
+                                new CurrentVPanel(new Fill(THUMBSTROKE), ALIEXTENT, 1,
+                                    "BLACK")
                             ]),
                         ]),
                         0,
@@ -3733,8 +3733,8 @@ var bodylst =
                                 new Rectangle(context.stretchctrl),
                                 new Fill(THUMBFILL),
                                 new Stroke(THUMBSTROKE,THUMBORDER),
-                                new CurrentVPanel(new Fill(j==1?THUMBDARK:THUMBSTROKE), ALIEXTENT, 1, 
-                                    j==1?"WHITE":"BLACK")
+                                new CurrentVPanel(new Fill(THUMBSTROKE), ALIEXTENT, 1, 
+                                    "BLACK")
                             ]),
                         ])
                     ]));
@@ -3765,8 +3765,8 @@ var bodylst =
                             new Rectangle(context.zoomctrl),
                             new Fill(THUMBFILL),
                             new Stroke(THUMBSTROKE,THUMBORDER),
-                            new CurrentHPanel(new Fill(j==0?THUMBDARK:THUMBSTROKE), ALIEXTENT, 
-                                j==0?"WHITE":"BLACK")
+                            new CurrentHPanel(new Fill(THUMBSTROKE), ALIEXTENT, 
+                                "BLACK")
                         ]),
                         new Text("white", "center", "middle",0,0,1),
                         new Layer(
@@ -3774,8 +3774,8 @@ var bodylst =
                             new Rectangle(context.stretchctrl),
                             new Fill(THUMBFILL),
                             new Stroke(THUMBSTROKE,THUMBORDER),
-                            new CurrentHPanel(new Fill(j==1?THUMBDARK:THUMBSTROKE), ALIEXTENT, 
-                                j==1?"WHITE":"BLACK"),
+                            new CurrentHPanel(new Fill(THUMBSTROKE), ALIEXTENT, 
+                                "BLACK"),
                         ]),
                     ]));
 
@@ -4740,7 +4740,7 @@ var CurrentHPanel = function (panel, extent, clr)
         context.shadowOffsetX = 0;
         context.shadowOffsetY = 0;
         var t = new Text(clr, "center", "middle",0, 0, 0);
-        //t.draw(context, r, (user.current()+1).toFixed(0), 0);
+        t.draw(context, r, (user.current()+1).toFixed(0), 0);
         context.restore();
     };
 };
@@ -4758,7 +4758,7 @@ var CurrentVPanel = function (panel, extent, rev, clr)
         context.shadowOffsetX = 0;
         context.shadowOffsetY = 0;
         var t = new Text(clr, "center", "middle",0, 0, 0);
-    //    t.draw(context, r, (user.current()+1).toFixed(0), 0);
+        t.draw(context, r, (user.current()+1).toFixed(0), 0);
         context.restore();
     };
 };
