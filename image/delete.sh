be2cdb8783164aecd3504550caefd559ef7f88de
Tom Brinkman <reportbase@gmail.com>
2023-02-24 09:38:56 -0700

    na

--- image/delete.sh
+++ image/delete.sh
@@ -1,7 +1,8 @@
-#./stats.sh HOME.0000
-curl --request DELETE \
---url https://api.cloudflare.com/client/v4/accounts/41f6f507a22c7eec431dbc5e9670c73d/image/v1/$1 \
--H "Authorization: Bearer majjgSB2awSS1-8WJ7OoRvst4gsGjfLl3Fl0kpdC" -H 'Content-Type: application/json' 
+#./delete.sh test
+
+curl -X DELETE \
+--url https://api.cloudflare.com/client/v4/accounts/41f6f507a22c7eec431dbc5e9670c73d/image/v1/test \
+-H "Authorization: Bearer hXCWi4iJ8wDztj3LUWqzqXyqjgPCmPypnr5Rjkjb" -H 'Content-Type: application/json' 
 
 
 
--- image/put.sh
+++ image/put.sh
@@ -1,9 +1,9 @@
 #./put.sh HOME.0000 jpg
 
-IMAGE=$(printf "file=@./$1.$2" "$i")
-ID=$(printf "id=$1" "$i")
+IMAGE=$(printf "file=@./$1.webp" "")
+ID=$1
 POST=$(printf "https://api.cloudflare.com/client/v4/accounts/%s/image/v1" "41f6f507a22c7eec431dbc5e9670c73d")
-HEADER=$(printf "Authorization: Bearer %s" "y0wMwbTTYZkJG9IDm3EngHYrnsdEqfaUPgc6J38R")
+HEADER=$(printf "Authorization: Bearer %s" "hXCWi4iJ8wDztj3LUWqzqXyqjgPCmPypnr5Rjkjb")
 curl --request POST $POST  --header "$HEADER" --form $IMAGE --form $ID --form 'metadata={"key":"value"}'
 
 
