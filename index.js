c30ad9ad1bda970e412c013d82195bbe638c3351
Tom Brinkman <reportbase@gmail.com>
2023-09-13 06:36:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5504,14 +5504,15 @@ galleryobj.init = function (obj)
 	
 	{title:"Text2Image", func: function()
 		{
+			text2image(
 			var raw =
 			{
 			    "key": "rlQ8Oid4VByAEC7pRh6Ilx1lnnv9VCL6eReAQyWNWDnMQB8V9mainfTRFmCs",
 			    "prompt": "ultra realistic close up portrait ((beautiful pale cyberpunk female with heavy black eyeliner)), blue eyes, shaved side haircut, hyper detail, cinematic lighting, magic neon, dark red city, Canon EOS R3, nikon, f/1.4, ISO 200, 1/160s, 8K, RAW, unedited, symmetrical balance, in-frame, 8K",
 			    "negative_prompt": "((out of frame)), ((extra fingers)), mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), (((tiling))), ((naked)), ((tile)), ((fleshpile)), ((ugly)), (((abstract))), blurry, ((bad anatomy)), ((bad proportions)), ((extra limbs)), cloned face, (((skinny))), glitchy, ((extra breasts)), ((double torso)), ((extra arms)), ((extra hands)), ((mangled fingers)), ((missing breasts)), (missing lips), ((ugly face)), ((fat)), ((extra legs))",
-			    "width": "512",
-			    "height": "512",
-			    "samples": "1",
+			    "width": "1024",
+			    "height": "1024",
+			    "samples": "4",
 			    "num_inference_steps": "20",
 			    "safety_checker": "no",
 			    "enhance_prompt": "yes",
@@ -5537,6 +5538,34 @@ galleryobj.init = function (obj)
 			      .then(function(json)
 				    {
 					    console.log(json)
+				 /*
+			 	{
+				  "status": "success",
+				  "generationTime": 1.3200268745422363,
+				  "id": 12202888,
+				  "output": [
+				    "https://pub-8b49af329fae499aa563997f5d4068a4.r2.dev/generations/e5cd86d3-7305-47fc-82c1-7d1a3b130fa4-0.png"
+				  ],
+				  "meta": {
+				    "H": 512,
+				    "W": 512,
+				    "enable_attention_slicing": "true",
+				    "file_prefix": "e5cd86d3-7305-47fc-82c1-7d1a3b130fa4",
+				    "guidance_scale": 7.5,
+				    "model": "runwayml/stable-diffusion-v1-5",
+				    "n_samples": 1,
+				    "negative_prompt": " ((out of frame)), ((extra fingers)), mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), (((tiling))), ((naked)), ((tile)), ((fleshpile)), ((ugly)), (((abstract))), blurry, ((bad anatomy)), ((bad proportions)), ((extra limbs)), cloned face, glitchy, ((extra breasts)), ((double torso)), ((extra arms)), ((extra hands)), ((mangled fingers)), ((missing breasts)), (missing lips), ((ugly face)), ((fat)), ((extra legs))",
+				    "outdir": "out",
+				    "prompt": "ultra realistic close up portrait ((beautiful pale cyberpunk female with heavy black eyeliner)) DSLR photography, sharp focus, Unreal Engine 5, Octane Render, Redshift, ((cinematic lighting)), f/1.4, ISO 200, 1/160s, 8K, RAW, unedited, symmetrical balance, in-frame",
+				    "revision": "fp16",
+				    "safetychecker": "no",
+				    "seed": 3499575229,
+				    "steps": 20,
+				    "vae": "stabilityai/sd-vae-ft-mse"
+				  }
+				}
+    				*/
+					    
 				    })
 			      .catch(error => console.log(error) ); 
 			            
