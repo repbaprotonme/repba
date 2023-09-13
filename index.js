07c9474820cd9bd614e9d2facd8f24ca789481b8
Tom Brinkman <reportbase@gmail.com>
2023-09-12 22:51:55 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,43 +7,7 @@ https://zip-view.com
 https://ipfs-view.com
 */
 
-var raw = "{\n    \"key\": \"rlQ8Oid4VByAEC7pRh6Ilx1lnnv9VCL6eReAQyWNWDnMQB8V9mainfTRFmCs\",\n    \"prompt\": \"ultra realistic close up portrait ((beautiful pale cyberpunk female with heavy black eyeliner)), blue eyes, shaved side haircut, hyper detail, cinematic lighting, magic neon, dark red city, Canon EOS R3, nikon, f/1.4, ISO 200, 1/160s, 8K, RAW, unedited, symmetrical balance, in-frame, 8K\",\n    \"negative_prompt\": \"((out of frame)), ((extra fingers)), mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), (((tiling))), ((naked)), ((tile)), ((fleshpile)), ((ugly)), (((abstract))), blurry, ((bad anatomy)), ((bad proportions)), ((extra limbs)), cloned face, (((skinny))), glitchy, ((extra breasts)), ((double torso)), ((extra arms)), ((extra hands)), ((mangled fingers)), ((missing breasts)), (missing lips), ((ugly face)), ((fat)), ((extra legs)), anime\",\n    \"width\": \"512\",\n    \"height\": \"512\",\n    \"samples\": \"1\",\n    \"num_inference_steps\": \"20\",\n    \"seed\": null,\n    \"guidance_scale\": 7.5,\n    \"webhook\": null,\n    \"track_id\": null\n}";
 
-var raw2 =
-{
-    "key": "rlQ8Oid4VByAEC7pRh6Ilx1lnnv9VCL6eReAQyWNWDnMQB8V9mainfTRFmCs",
-    "prompt": "ultra realistic close up portrait ((beautiful pale cyberpunk female with heavy black eyeliner)), blue eyes, shaved side haircut, hyper detail, cinematic lighting, magic neon, dark red city, Canon EOS R3, nikon, f/1.4, ISO 200, 1/160s, 8K, RAW, unedited, symmetrical balance, in-frame, 8K",
-    "negative_prompt": "((out of frame)), ((extra fingers)), mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), (((tiling))), ((naked)), ((tile)), ((fleshpile)), ((ugly)), (((abstract))), blurry, ((bad anatomy)), ((bad proportions)), ((extra limbs)), cloned face, (((skinny))), glitchy, ((extra breasts)), ((double torso)), ((extra arms)), ((extra hands)), ((mangled fingers)), ((missing breasts)), (missing lips), ((ugly face)), ((fat)), ((extra legs))",
-    "width": "512",
-    "height": "512",
-    "samples": "1",
-    "num_inference_steps": "20",
-    "safety_checker": "no",
-    "enhance_prompt": "yes",
-    "seed": null,
-    "guidance_scale": 7.5,
-    "webhook": null,
-    "track_id": null
-}
-
-var requestOptions = 
-{
-  method: 'POST',
-  body: raw,
-  redirect: 'follow',
-  "headers": 
-   {
-      "Content-Type": "application/json",
-    },
-};
-
-fetch("https://stablediffusionapi.com/api/v3/text2img", requestOptions)
-      .then(response => texthandler(response))
-      .then(function(text)
-	    {
-		    console.log(text)
-	    })
-      .catch(error => console.log(error) );
 
 function iOS()
 {
@@ -5537,7 +5501,49 @@ galleryobj.init = function (obj)
             },
             enabled: function() { return 0; }
         },
-	    
+	
+	{title:"Text2Image", func: function()
+		{
+			var raw =
+			{
+			    "key": "rlQ8Oid4VByAEC7pRh6Ilx1lnnv9VCL6eReAQyWNWDnMQB8V9mainfTRFmCs",
+			    "prompt": "ultra realistic close up portrait ((beautiful pale cyberpunk female with heavy black eyeliner)), blue eyes, shaved side haircut, hyper detail, cinematic lighting, magic neon, dark red city, Canon EOS R3, nikon, f/1.4, ISO 200, 1/160s, 8K, RAW, unedited, symmetrical balance, in-frame, 8K",
+			    "negative_prompt": "((out of frame)), ((extra fingers)), mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), (((tiling))), ((naked)), ((tile)), ((fleshpile)), ((ugly)), (((abstract))), blurry, ((bad anatomy)), ((bad proportions)), ((extra limbs)), cloned face, (((skinny))), glitchy, ((extra breasts)), ((double torso)), ((extra arms)), ((extra hands)), ((mangled fingers)), ((missing breasts)), (missing lips), ((ugly face)), ((fat)), ((extra legs))",
+			    "width": "512",
+			    "height": "512",
+			    "samples": "1",
+			    "num_inference_steps": "20",
+			    "safety_checker": "no",
+			    "enhance_prompt": "yes",
+			    "seed": null,
+			    "guidance_scale": 7.5,
+			    "webhook": null,
+			    "track_id": null
+			}
+			
+			var requestOptions = 
+			{
+			  method: 'POST',
+			  body: JSON.stringify(raw),
+			  redirect: 'follow',
+			  "headers": 
+			   {
+			      "Content-Type": "application/json",
+			    },
+			};
+			
+			fetch("https://stablediffusionapi.com/api/v3/text2img", requestOptions)
+			      .then(response => jsonhandler(response))
+			      .then(function(json)
+				    {
+					    console.log(json)
+				    })
+			      .catch(error => console.log(error) ); 
+			            
+	      },
+	     enabled: function() { return 0; }
+        },
+	
 	{title:"Full Screen", path: "FULLSCREEN", func: function()
             {
                 if (screenfull.isEnabled)
