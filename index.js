e65907a4343cea20c5e0327ddd3ed7751eafa4aa
Tom Brinkman <reportbase@gmail.com>
2023-09-12 22:36:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ https://zip-view.com
 https://ipfs-view.com
 */
 
-var raw =
+var raw = "{\n    \"key\": \"rlQ8Oid4VByAEC7pRh6Ilx1lnnv9VCL6eReAQyWNWDnMQB8V9mainfTRFmCs\",\n    \"prompt\": \"ultra realistic close up portrait ((beautiful pale cyberpunk female with heavy black eyeliner)), blue eyes, shaved side haircut, hyper detail, cinematic lighting, magic neon, dark red city, Canon EOS R3, nikon, f/1.4, ISO 200, 1/160s, 8K, RAW, unedited, symmetrical balance, in-frame, 8K\",\n    \"negative_prompt\": \"((out of frame)), ((extra fingers)), mutated hands, ((poorly drawn hands)), ((poorly drawn face)), (((mutation))), (((deformed))), (((tiling))), ((naked)), ((tile)), ((fleshpile)), ((ugly)), (((abstract))), blurry, ((bad anatomy)), ((bad proportions)), ((extra limbs)), cloned face, (((skinny))), glitchy, ((extra breasts)), ((double torso)), ((extra arms)), ((extra hands)), ((mangled fingers)), ((missing breasts)), (missing lips), ((ugly face)), ((fat)), ((extra legs)), anime\",\n    \"width\": \"512\",\n    \"height\": \"512\",\n    \"samples\": \"1\",\n    \"num_inference_steps\": \"20\",\n    \"seed\": null,\n    \"guidance_scale\": 7.5,\n    \"webhook\": null,\n    \"track_id\": null\n}";
+
+var raw2 =
 {
     "key": "rlQ8Oid4VByAEC7pRh6Ilx1lnnv9VCL6eReAQyWNWDnMQB8V9mainfTRFmCs",
     "prompt": "ultra realistic close up portrait ((beautiful pale cyberpunk female with heavy black eyeliner)), blue eyes, shaved side haircut, hyper detail, cinematic lighting, magic neon, dark red city, Canon EOS R3, nikon, f/1.4, ISO 200, 1/160s, 8K, RAW, unedited, symmetrical balance, in-frame, 8K",
@@ -27,7 +29,7 @@ var raw =
 var requestOptions = 
 {
   method: 'POST',
-  body: JSON.stringify(raw),
+  body: raw,
   redirect: 'follow'
 };
 
