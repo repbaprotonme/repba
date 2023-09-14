b53c4a5a2fa2999fcb5a1e56a78f93f4f9734bc8
Tom Brinkman <reportbase@gmail.com>
2023-09-13 20:11:57 -0600

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ const TIMEOBJ = 3927;
 const SCROLLMARGIN = 8;
 const MENUSELECT = "rgba(255,175,0,0.4)";
 const MENUTAP = "rgba(255,125,0,0.7)";
-const SELECTAP = "rgba(255,0,0.75,0.7)";
 const SCROLLNAB = "rgba(0,0,0,0.3)";
 const DARKNAB = "rgba(0,0,0,0.85)";
 const MENUCOLOR = "rgba(0,0,0,0.5)";
