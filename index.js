4b9d1e5880877baf5aa79a334b48b6640ffe72c0
Tom Brinkman <reportbase@gmail.com>
2023-09-14 16:33:39 -0600

    Update index.js

--- index.js
+++ index.js
@@ -2593,7 +2593,8 @@ var keylst =
 		var k = TIMEOBJ - TIMEOBJ/galleryobj.length()/2;//centered
 		if (buttonobj.value() > window.innerHeight)
 		{
-			var e = window.innerHeight/_8cnv.virtualheight/2;
+			var m = window.innerHeight/2;
+			var e = m/_8cnv.virtualheight;
 			j = e*TIMEOBJ;
 		}
 		
