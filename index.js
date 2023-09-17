7e6ac72f7f79a010f2a53b4f12be80c6cca1d425
Tom Brinkman <reportbase@gmail.com>
2023-09-16 20:04:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,48 @@ https://zip-view.com
 https://ipfs-view.com
 */
 
+function text2image()
+{
+	var myHeaders = new Headers();
+	myHeaders.append("Content-Type", "application/json");
+	
+	var raw = JSON.stringify({
+	  "key": "rlQ8Oid4VByAEC7pRh6Ilx1lnnv9VCL6eReAQyWNWDnMQB8V9mainfTRFmCs",
+	  "prompt": "ultra realistic close up portrait ((beautiful pale cyberpunk female with heavy black eyeliner))",
+	  "negative_prompt": null,
+	  "width": "1024",
+	  "height": "1024",
+	  "samples": "1",
+	  "num_inference_steps": "20",
+	  "seed": null,
+	  "guidance_scale": 7.5,
+	  "safety_checker": "yes",
+	  "multi_lingual": "no",
+	  "panorama": "no",
+	  "self_attention": "no",
+	  "upscale": "no",
+	  "embeddings_model": null,
+	  "webhook": null,
+	  "track_id": null
+	});
+	
+	var requestOptions = {
+	  method: 'POST',
+	  headers: myHeaders,
+	  body: raw,
+	  redirect: 'follow'
+	};
+	
+	fetch("https://stablediffusionapi.com/api/v3/text2img", requestOptions)
+	  .then(response => response.json())
+	  .then(function(json)
+	   {
+		   console.log(json)
+	   })
+	  .catch(error => console.log('error', error));
+	}
+}
+
 function iOS()
 {
     return
