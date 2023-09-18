fa1692e3337f63c3cd743e86f9d4943de5d0534e
Tom Brinkman <reportbase@gmail.com>
2023-09-17 18:42:06 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5934,7 +5934,7 @@ function selectname(name)
     for (var m = 0; m < galleryobj.data.length; ++m)
     {
         var e = galleryobj.data[m];
-        if ((e.name && !e.name.wild(name)) 
+        if (!e.name || !e.name.wild(name)) 
 	    continue;
 	gotoimage(m);
         localobj.time = _8cnv.timeobj.current();
