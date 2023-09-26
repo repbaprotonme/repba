dda52d397543f3c33b2aa2d98679fb1ef6a96ae5
Tom Brinkman <reportbase@gmail.com>
2023-09-26 08:26:15 -0600

    Update index.js

--- index.js
+++ index.js
@@ -606,13 +606,17 @@ panel.galleryscroll = function ()
         var a = new panel.col([0,SCROLLBARWIDTH,2],
             [
                 0,
-                new panel.row([5,0,5],
+                new panel.row([6,0,6],
                 [
                     0,
 			new Layer(
 			[
-        	    		new panel.expand(new panel.rectangle(canvas.vscrollrect),10,0),
-				new panel.currentV(new panel.shadow(new panel.fill("white")), 90, 1),
+				new Fill("rgba(0,0,0,0.2)"),
+				new Layer(
+				[
+	        	    		new panel.expand(new panel.rectangle(canvas.vscrollrect),10,0),
+					new panel.currentV(new panel.shadow(new panel.fill("white")), 90, 1),
+				]),
 			]),
                     0,
                 ]),
@@ -624,14 +628,18 @@ panel.galleryscroll = function ()
         var a = new panel.row([0,SCROLLBARWIDTH,2],
             [
                 0,
-                new panel.col([5,0,5],
+                new panel.col([6,0,6],
                 [
                     0,
 		    new Layer(
-		    [
-        	    	new panel.expand(new panel.rectangle(canvas.hscrollrect),0,10),
-			    new panel.currentH(new panel.shadow(new panel.fill("white")), 90, 1),
-		    ]),
+		    [ 
+			    new Fill("rgba(0,0,0,0.2)"),
+			    new Layer(
+			    [
+	        	    	new panel.expand(new panel.rectangle(canvas.hscrollrect),0,10),
+				    new panel.currentH(new panel.shadow(new panel.fill("white")), 90, 1),
+			    ]),
+		   ]),
                     0,
                 ]),
                 0,
@@ -653,7 +661,7 @@ panel.scrollbar = function ()
         var a = new panel.col([0,SCROLLBARWIDTH,5],
             [
                 0,
-                new panel.row([5,0,5],
+                new panel.row([6,0,6],
                 [
                     0,
                     new Layer(
@@ -2358,7 +2366,11 @@ var presslst =
         }
         else
         {
-        }
+               var h = headcnv.height?0:BEXTENT;
+            headcnvctx.show(0,0,window.innerWidth,h);
+            headobj.value().draw(headcnvctx, headcnvctx.rect(), 0);
+            context.refresh()
+	}
     },
     press: function (context, rect, x, y)
     {
@@ -5663,9 +5675,9 @@ function initime()
 
 url.path = "home";
 
-if (url.searchParams.has("ipfs"))
+if (url.searchParams.has("qid"))
 {
-    url.path = url.searchParams.get("ipfs");
+    url.path = url.searchParams.get("qid");
 var path2 = `https://dweb.link/ipfs/${url.path}`;
 var path1 = `https://ipfs.filebase.io/ipfs/${url.path}`;
 var path3 = `https://dweb.link/api/v0/ls?arg=${url.path}`
