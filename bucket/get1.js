a4e77ad08fa573243465254a1b6368a43ecc7c6a
Tom Brinkman <reportbase@gmail.com>
2023-10-08 16:09:18 -0600

    Delete bucket directory

--- bucket/get1.js
+++
@@ -1,6 +0,0 @@
-return fetch(`https://bucket.reportbase5836.workers.dev/put1.sh`)
-  .then(response => response.text())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
-
-
